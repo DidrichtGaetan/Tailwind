f6799ab5cf6ab70acf1696e56d885e9db730d08b
Gaetan <g.didricht@chba.be>
2023-04-20 16:06:42 +0200

    project commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      minHeight: {
+        500: "350px",
+        650: "500px"
+      }
+    },
+  },
+  plugins: [],
+}
