f68defef8a93f3f5cc5bd380f7a33db43d707fd0
Gaetan <g.didricht@chba.be>
2023-08-30 15:17:56 +0200

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Attendez que le document soit prêt
+  
+    // Sélectionnez l'élément du logo
+    var logo = $("#logo");
+  
+    // Ajoutez la classe pour l'effet d'apparition
+    logo.addClass("fade-in");
+  });
