c302f7f159f16fc51a3be0b41b7f4b2cdaa409f4
Gaetan <g.didricht@chba.be>
2023-12-19 14:49:28 +0100

    add scroll animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,12 @@
 /* @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
-  relative: true,
-    transform: (content) => content.replace(/taos:/g, ''),
-    files: ['./src/*.{html,js}'],
   theme: {
-    extend: {
-      minHeight: {
-        500: "350px",
-        650: "500px"
-      }
-    },
+    extend: {},
   },
-  plugins: [
-    require('taos/plugin')
-  ],
-  safelist: [
-    '!duration-[0ms]',
-    '!delay-[0ms]',
-    'html.js :where([class*="taos:"]:not(.taos-init))'
-  ]
+  plugins: [],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ['./*.{html,js}'],
+  },
 }
