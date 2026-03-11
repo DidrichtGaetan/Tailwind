ddb11fd01458e98ba604d87bfa0985522340ba49
DidrichtGaetan <60644411+DidrichtGaetan@users.noreply.github.com>
2026-03-11 20:06:23 +0100

    changement graphique

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
-/* @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nav-white': '#FFFFFF',
+        'main-white': '#dddddd',
+        'shade-white': '#d3d3d3',
+        'text-color': '#333',
+        'main-black': '#181818',
+        'main-yellow': '#CBB26A',
+        'dark-yellow': '#f4cb38',
+        'lite-yellow': '#FEF7E0',
+        'lite-gris': '#F8F9FA',
+        'bg-color': '#fdfdfd',
+        'hero-mint': '#4FF3C3', // More vibrant mint from screenshot
+        'hero-blue': '#002B49',
+        'hero-gray': '#718096', // Softer gray for descriptions
+        'hero-yellow': '#CCF98C', // Lime/Yellow for gradients
+      },
+      fontFamily: {
+        'base': ['"Open Sans"', 'sans-serif'],
+        'title': ['Merriweather', 'serif'],
+        'title-main': ['"Dancing Script"', 'cursive'],
+        'title-rubrique': ['Gagalin', 'sans-serif'],
+        'texte': ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
   content: {
--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,34 @@ module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
+        // Version espagnole
         main: './index.html',
         particulares: './clasesParticulares.html',
         grupo: './enGrupoReducido.html',
-        online: './online.html',
         empresas: './enEmpresas.html',
-        espagnol: 'espagnol.html',
-        francais: 'francais.html',
-        anglais: 'anglais.html',
-        traduccion: 'traduccion.html',
-        methodologie: 'methodologie.html'
+        espagnol: './espagnol.html',
+        francais: './francais.html',
+        anglais: './anglais.html',
+        traduccion: './traduccion.html',
+        methodologie: './methodologie.html',
+        online: './online.html',
+        intensivo: './intensivo.html',
+        offres: './offres.html',
+        anoUno: './1ano.html',
+        // Version française
+        main_fr: './index_fr.html',
+        particulares_fr: './clasesParticulares_fr.html',
+        grupo_fr: './enGrupoReducido_fr.html',
+        empresas_fr: './enEmpresas_fr.html',
+        espagnol_fr: './espagnol_fr.html',
+        francais_fr: './francais_fr.html',
+        anglais_fr: './anglais_fr.html',
+        traduccion_fr: './traduccion_fr.html',
+        methodologie_fr: './methodologie_fr.html',
+        online_fr: './online_fr.html',
+        intensivo_fr: './intensivo_fr.html',
+        offres_fr: './offres_fr.html',
+        anoUno_fr: './1ano_fr.html',
       }
     }
   }
